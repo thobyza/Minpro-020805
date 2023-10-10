93bf342838f43109d099ca5e6a5186fa06564610
thoby za <99469497+thobyza@users.noreply.github.com>
2023-10-10 14:37:48 +0700

    Mp ta 4 (#6)
    * navbar
    
    * navbar
    
    * navbar update

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,12 +1,12 @@
-import { BottomMenu } from "../components/bottomMenu"
-import { Navbar } from "../components/navbar"
+import { BottomMenu } from "../components/bottomMenu";
+import { Navbar } from "../components/navbar";
 
 export const HomePage = () => {
-    return (
-        <>
-            <Navbar/>
-            <div className="bg-indigo-200 h-screen"></div>
-            <BottomMenu/>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <div className="h-screen bg-indigo-200"></div>
+      <BottomMenu />
+    </>
+  );
+};
