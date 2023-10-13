07427ec409671a5d7ab323a0c8ebc908e32fef63
thoby za <thobyy.athaya@gmail.com>
2023-10-13 09:18:15 +0700

    landing page create

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -1,11 +1,12 @@
 import { BottomMenu } from "../components/bottomMenu";
+import LandingSection from "../components/landingSection";
 import { Navbar } from "../components/navbar";
 
 export const HomePage = () => {
   return (
     <>
       <Navbar />
-      <div className="h-screen bg-indigo-200"></div>
+      <LandingSection/>
       <BottomMenu />
     </>
   );
